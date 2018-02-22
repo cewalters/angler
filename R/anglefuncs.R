12fd4e4eed185b6ad4dcb0b1f4205f73dbbcbc55
Caroline Walters <c.e.walters6@gmail.com>
2018-02-22 12:42:04 +0000

    wrote some documentation

--- R/anglefuncs.R
+++ R/anglefuncs.R
@@ -1,5 +1,26 @@
 
+
+#' Find the hypotenuse of a right-angled triangle in Euclidean space from the
+#' other two sides.
+#' @param opposite A number.
+#' @param adjacent A number.
+#' @return The hypotenuse of the right-angled triangle with sides
+#' \code{opposite} and \code{adjacent}.
+#' @examples
+#' hypotenuse(1, 1)
+#' hypotenuse(10, 1)
+
+
 hypotenuse <- function(opposite, adjacent){ sqrt(opposite^2 + adjacent^2)}
+
+#' Find the angle between adjacent and hypotenuse of a right-angled triangle in
+#' Euclidean space.
+#' @param opposite A number.
+#' @param adjacent A number.
+#' @return The angle between side \code{adjacent} and the hypotenuse.
+#' @examples
+#' theta(1, 1)
+#' theta(10, 1)
 
 theta <- function(opposite, adjacent){
   opp <- opposite
@@ -8,6 +29,16 @@ theta <- function(opposite, adjacent){
 
   return(theta)
 }
+
+
+#' Find the angle between opposite and hypotenuse of a right-angled triangle in
+#' Euclidean space.
+#' @param opposite A number.
+#' @param adjacent A number.
+#' @return The angle between side \code{opposite} and the hypotenuse.
+#' @examples
+#' phi(1, 1)
+#' phi(10, 1)
 
 phi <- function(opposite, adjacent){
   pi/2 - theta(opposite, adjacent)

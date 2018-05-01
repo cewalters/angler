6a10018fee98f0455da625f1f372b6540c25bf2b
Caroline Walters <c.e.walters6@gmail.com>
2018-05-01 12:32:38 +0100

    wrote a new test with expect_error

--- tests/testthat/test_hypotenuse.R
+++ tests/testthat/test_hypotenuse.R
@@ -3,4 +3,14 @@ library(angler)
 
 test_that("hypotenuse obey pythag", {
   expect_equal(hypotenuse(opposite = 2, adjacent = 3), sqrt(2^2 + 3^2))
+  expect_equal(hypotenuse(opposite = 2, adjacent = 3), sqrt(13))
+  expect_equal(hypotenuse(opposite = 3, adjacent = 4), 5)
+})
+
+test_that("hypotenuse errors",{
+  expect_error(hypotenuse(opposite = -2, adjacent = 3))
+  expect_error(hypotenuse(opposite = 2, adjacent = -3))
+  expect_error(hypotenuse(opposite = -2, adjacent = -3))
+  expect_error(hypotenuse(opposite = 0, adjacent = 3))
+  expect_error(hypotenuse(opposite = 2, adjacent = 0))
 })
